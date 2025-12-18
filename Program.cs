5e76527c662e496ac7a7b339de9f851087b07ea6
Filip <fihu08001@utb.vaxjo.se>
2025-12-18 14:21:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Uppgift_4._9
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många X per grupp?");
+            int antalX = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O per grupp");
+            int antalO = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O-mönster per rad?");
+            int antalOmönster = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många rader?");
+            int rader = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rader; i++)
+            {
+                for (int j = 0; j < antalOmönster; j++)
+                {
+                    for (int k = 0; k < antalX; k++)
+                    {
+                        Console.Write("X");
+                    }
+                    for (int l = 0; l < antalO; l++)
+                    {
+                        Console.Write("O");
+                    }
+                }
+                Console.WriteLine();
+            }
+       
+                    
+                
+                
+            
+        }
+            
+    }
+}
